b8420655195b8bafb8c941faed8fca37d96fa590
1knn <1kn.evie@gmail.com>
2021-03-16 20:46:15 -0300

    iniciando processo de armazenar dados

---
+++ proj-ASP.NET MVC/QuintaApp/Controllers/FuncionarioController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuintaApp.Models;
+
+namespace QuintaApp.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        // GET: Funcionario
+        public ActionResult Index()
+        {
+            Funcionario funcionario = new Funcionario()
+            {
+                FuncID = 1,
+                FuncNome = "Bruno",
+                FuncFuncao = "Sênior"
+            };
+
+            return View(funcionario);
+        }
+
+        public ActionResult Listar(ushort A, string FuncNome, string FuncFuncao )
+        {
+            ViewData["FuncID"] = A;
+            ViewData["FuncNome"] = FuncNome;
+            ViewData["FuncFuncao"] = FuncFuncao;
+
+            return View();
+        }
+    }
+}
