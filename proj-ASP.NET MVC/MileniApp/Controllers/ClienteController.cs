1c141a791fdfb4d0103d6afafecb431386316254
1knn <1kn.evie@gmail.com>
2021-03-16 20:47:40 -0300

    finalizado: projeto basico avaliativo asp.net aula 16-4-21

---
+++ proj-ASP.NET MVC/MileniApp/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using MileniApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MileniApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            Cliente cliente = new Cliente()
+            {
+                ClienteID = 1,
+                ClieteNome = "Bela",
+                Cpf = 12345678911,
+                RG = 123445678,
+                DigRg = '0',
+                ClieteTel = 12345678
+                
+            };
+
+            return View(cliente);
+        }
+    }
+}
---
+++ proj-ASP.NET MVC/MileniApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MileniApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "A app tem como objetivo demonstrar todos os conhecimentos adquiridos na disciplina Desenvolvimento de Software";
+
+            return View();
+        }
+
+        public ActionResult MileniApp()
+        {
+
+
+            return View();
+        }
+        
+    }
+}
